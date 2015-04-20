92bc2c5a8dea4eb696b86e0a67275f5e8c0dfcbb
Eric Wiese <eric.wiese@classyllama.com>
2015-04-19 19:01:17 -0500

    POC implementation of system config inclusion in global search

---
+++ app/code/Magento/Config/Model/Search/Config.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Magento\Config\Model\Search;
+use Magento\Search\Model\QueryFactory;
+
+class Config extends \Magento\Framework\Object
+{
+    /** @var \Magento\Backend\Helper\Data */
+    protected $_adminhtmlData;
+    /** @var \Magento\Framework\Stdlib\String  */
+    protected $_string;
+    /** @var array */
+    protected $_configStructureData;
+
+    /**
+     * @param \Magento\Backend\Helper\Data $adminhtmlData
+     * @param \Magento\Framework\Stdlib\String $string
+     * @param \Magento\Config\Model\Config\Structure\Data $configStructureData
+     * @param QueryFactory $queryFactory
+     */
+    public function __construct(
+        \Magento\Backend\Helper\Data $adminhtmlData,
+        \Magento\Framework\Stdlib\String $string,
+        \Magento\Config\Model\Config\Structure\Data $configStructureData
+    ) {
+        $this->_adminhtmlData = $adminhtmlData;
+        $this->_string = $string;
+        $this->_configStructureData = $configStructureData->get();
+    }
+
+    /**
+     * Load search results
+     *
+     * @return $this
+     */
+    public function load()
+    {
+        $result = [];
+        if (!$this->hasStart() || !$this->hasLimit() || !$this->hasQuery()) {
+            $this->setResults($result);
+            return $this;
+        }
+
+        $tabs = $this->_configStructureData['tabs'];
+
+        $fields = [];
+        foreach($this->_configStructureData['sections'] as $section) {
+            foreach($section['children'] as $group) {
+                if(!isset($group['children'])) {
+                    continue;
+                }
+                foreach($group['children'] as $field) {
+                    $field['tab'] = $tabs[$section['tab']]['label'];
+                    $field['section'] = $section['label'];
+                    $field['group'] = $group['label'];
+                    $fields[] = $field;
+                }
+            }
+        }
+
+        foreach($fields as $field) {
+            if(!isset($field['label'])) {
+                continue;
+            }
+
+            $label = strtolower($field['label']);
+            $query = strtolower($this->getQuery());
+
+            if(strpos($label, $query) !== false) {
+                $result[] = [
+                    'id' => 'store-config-search',
+                    'type' => __('Store Config Field'),
+                    'name' => __($field['label']),
+                    'description' => sprintf(
+                        '%s -> %s -> %s',
+                        __($field['tab']),
+                        __($field['section']),
+                        __($field['group'])
+                    ),
+                    'url' => $this->_adminhtmlData->getUrl(
+                        'admin/system_config/edit',
+                        array(
+                            'section' => $field['section']
+                        )
+                    ),
+                ];
+
+                if(isset($field['comment'])) {
+
+                }
+            }
+        }
+
+        $this->setResults($result);
+
+        return $this;
+    }
+}
